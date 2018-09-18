c128dde974e16d96f20513d7de385096ff08e4e4
kl2ag1 <gabszab@vp.pl>
2018-09-18 13:53:30 +0200

    .

---
+++ python/abc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  abc.py
+
+
+
+def main(args):
+    a = int(input("Podaj 1. liczbę: "))
+    print(a)
+    b = int(input("Podaj 2. liczbę: "))
+    print(b)
+
+    if a > b:
+        print(a, "jest większe od", b)
+    if a > b:
+        print(a, "jest mniejsze od", b)
+    if a > b:
+        print(a, "jest równe od", b)
+
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
