698003412bc953c414c73550346639d964eb9f82
kl2ag1 <gabszab@vp.pl>
2018-10-10 11:38:55 +0200

    nwd

---
+++ python/nwd_euklides.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  nwd_euklides.py
+
+def nwd_klasyczny(a, b):
+    
+    return nwd
+
+    
+def main(args):
+    
+    print(nwd_klasyczny(3, 190))
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
--- python/silnia.py
+++ python/silnia.py
@@ -7,19 +7,21 @@ def silnia_it(n):
     # 0! = 1
     # n! = 1 * 2 * ... * n
     wynik = 1
-    for i in range(n):
-        wynik = wynik + ! * n
+    for i in range(1, n + 1):
+        wynik = wynik * i
+        print(wynik)
     return wynik
     
 def main(args):
     # zmienne lokalne
-    #n = int(input("Podaj liczbę: "))
+    n = int(input("Podaj liczbę: "))
+    silnia_it(n)
     #print("{}! = {}". format(n, silnia_it(n)))
-    assert(silnia_it(0) == 1)
-    assert(silnia_it(1) == 1)
-    assert(silnia_it(4) == 24)
-    assert(silnia_it(5) == 120)
-    return 0
+    #assert(silnia_it(0) == 1)
+    #assert(silnia_it(1) == 1)
+    #assert(silnia_it(4) == 24)
+    #assert(silnia_it(5) == 120)
+    #return 0
 
 if __name__ == '__main__':
     import sys
