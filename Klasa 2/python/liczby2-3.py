415120e6c346cf517cdcab48c45d3c659bd67ee6
kl2ag1 <gabszab@vp.pl>
2018-10-17 11:40:26 +0200

    a

--- Klasa 2/python/liczby2-3.py
+++ Klasa 2/python/liczby2-3.py
@@ -5,7 +5,7 @@
 # 
 
 
-def liczby2(a=10, b=99):
+def liczby2():
     
     """
     Program drukuje wszystkie liczby dwucyfrowe o niepowtarzających się
@@ -21,13 +21,21 @@ def liczby2(a=10, b=99):
                 ile +=1
     return ile          
     
-def liczby3(a=100, b=999):
+def liczby3():
     
     """
     Program drukuje wszystkie liczby trzycyfrowe o niepowtarzających się
     cyfrach oraz zwraca ilość takich liczb
     """
+    ile = 0
     
+    for i in range(1, 10):
+        for j in range(0, 10):
+            if i!=j:
+                print("{}{} ".format(i, j), end="")
+                ile +=1
+    return ile 
+
 def main(args):
     
     print(liczby2())
