a53d27db5673261e2f8dc4b69f1ff7495a319814
KaynMain <32056078+KaynMain@users.noreply.github.com>
2018-11-07 01:45:52 +0100

    Update anagram.cpp

--- Klasa 2/cpp/anagram.cpp
+++ Klasa 2/cpp/anagram.cpp
@@ -1,41 +1,33 @@
 /*
  * anagram.cpp
- * 
- * Ala ma kota, a kot ma Alę!
- * alA am atok, a tok am ęlA!
  */
-
 
 #include <iostream>
 
 using namespace std;
 
-// anagram() - funkcja odwraca znaki w pojedyńczym wyrazie
-// wyrazy() - funkcja dzieli tekst na wyrazy
-
 int zlicz(char tb[]) {
     int i = 0;
-    while(tb[i] != '\0') i++;
-    return i;
-}
-
-void wyswietl(char tekst[], int roz) {
-    for(int i = 0; i < roz; i++) {
-        cout << tekst[i];
+     while(tb[i] != '\0') i++;
+     return i;
+ }
+ 
+ void wyswietl(char tekst[], int rozmiar) {
+     for(int i = 0; i < rozmiar; i++) {
+         cout << tekst[i];
     }
 }
 
 void anagram(char tb[], int roz) {
-    for(int i = roz - 1; i >= 0; i--) {
+    //for zaczyna od ostatniego znaku
+    for (int i = roz - 1; i >= 0; i--) {
         cout << tb[i];
-    }
+        }
 }
 
-int main(int argc, char **argv)
-{
-	const int roz = 50;
-    char tekst[roz]; 
-    cout << "Podaj wyraz: ";
+int main(int argc, char **argv){
+    const int roz = 50; 
+    char tekst[roz];
     cin.getline(tekst, roz);
     wyswietl(tekst, cin.gcount());
     cout << endl;
@@ -43,4 +35,4 @@ int main(int argc, char **argv)
     cout << endl;
 	return 0;
 }
-
+ 
