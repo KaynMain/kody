b91f03f715db4e895eef1e3c7d6d8c0eb4855877
kl2ag1 <gabszab@vp.pl>
2018-12-04 14:20:06 +0100

    .

--- Klasa 2/cpp/horner.cpp
+++ Klasa 2/cpp/horner.cpp
@@ -5,7 +5,6 @@
  * w(x) = 2x^3 + 3x^2+ 5x + 4 -- 6 mnożeń i 3 dodawania
  * w(x) = x(2x^2 + 3x + 5) + 4
  * w(x) = x(x(2x+3)+ 5) + 4 -- 3 mnożenia i 3 dodawania
- * wskaźnik - zmienna przechowywująca adres komórki w pamięci
  * 
  */
  
@@ -30,14 +29,19 @@ float horner_it(int st, float tbwsp[], float x){
     return wynik;
 }
 
+float horner_re(int st, float tbwsp[], float x){
+    if (st==0) return tbwsp[0];
+    return horner_re(st -1, tbwsp, x) * x + tbwsp[st];
+}
+
+
 int main(int argc, char **argv)
 {
-    
 	int stopien = 0;
     float x = 0;
     cout << "Podaj stopień wielomianu: ";
     cin >> stopien;
-    float *tbwsp; // wskaźnik
+    float *tbwsp; // wskaźnik - zmienna przechowywująca adres komórki w pamięci
     tbwsp = new float [stopien+1];
     for (int  i = 0; i <= stopien; i++) {
         cout << "Podaj współczynnik przy potędze " << stopien-i << ": ";
@@ -47,9 +51,15 @@ int main(int argc, char **argv)
     cout << "Podaj argument: ";
     cin >> x;
     
-    cout << "Wartość wielomianu o postaci: ";
+    cout << "Wartość iteracyjnie: ";
     drukujw(stopien, tbwsp);
     cout << "\ndla x = " << x << " wynosi: " << horner_it(stopien, tbwsp, x);
     cout << endl;
+    
+    cout << "Wartość rekurencyjnie: ";
+    drukujw(stopien, tbwsp);
+    cout << "\ndla x = " << x << " wynosi: " << horner_re(stopien, tbwsp, x);
+    cout << endl;
+    
 	return 0;
 }
