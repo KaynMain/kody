75a1f33422c6f187518dde122ffd6f03b4a9a41e
kl2ag1 <gabszab@vp.pl>
2018-12-11 14:09:38 +0100

    .

--- Klasa 2/cpp/sortowanie.cpp
+++ Klasa 2/cpp/sortowanie.cpp
@@ -1,53 +1,71 @@
 /*
  * sortowanie.cpp
- * 
  */
 
 
 #include <iostream>
-
 using namespace std;
 
-void wypelnij_losowe(int tab[], int roz) {   
-    for (int i = 0; i < roz; i++) {
-        tab[i] = rand() % 101;  
+void wypelnij_los(int tab[], int n) {
+    srand(time(NULL));//inicjacja generatora liczb pseudolosowych
+    for(int i = 0; i < n; i++) {
+        tab[i] = rand() % 101;
     }
 }
 
-void drukuj(int tab[], int roz) {    
-    for (int i = 0; i < roz; i++) {
+void drukuj(int tab[], int n) {
+    for(int i = 0; i < n; i++) {
         cout << tab[i] << " ";
     }
 }
 
-void zamien(int a, int b) {
-    int tmp;
-    a =b;
+void zamien(int &a, int &b){
+    int tmp = a;
+    a = b;
     b = tmp;
 }
 
-void sort_bubble(int tab[], int n) {
+void zamien2(int tab[], int i){
+    int tmp;
+    tmp = tab[i];
+    tab[i] = tab[i + 1];
+    tab[i + 1] = tmp;
+}
+void sort_bubble(int tab[], int n){
     cout << "\nSortowanie bąbelkowe\n";
-    for (int j= n - 1; j > 0 ; j--) {
+    int licznik = 0;
+    for (int j = n - 1; j > 0; j--) {
         for(int i = 0; i < j; i++) {
-            if tab[i]>tab[i+1]
-            zamien(tab[i], tab[i+1]);
+            licznik++;
+            if (tab[i]>tab[i-1])
+                zamien(tab[i], tab[i-1]);
         }
     }
+    cout << "\nPowtórzeń: " << licznik << endl;
 }
 
+void sort_insert(int tab, int n) {
+    cout << "\nSortowanie przez wstawianie";
+    int i, j, tmp;
+    for (i = 1; i <n; i++) { // pętla wybiera kolejne elementy zaczynając od drugiego
+        
+    }
+}
 int main(int argc, char **argv)
 {
-	int rozmiar = 20;
-    int tablica[rozmiar]; //statyczna deklaracja tablicy
+	int n = 20;
+    int tab[n];
+    wypelnij_los(tab, n);
+    drukuj(tab, n);
+    //int a = 10;
+    //int b = 20;
+    //zamien(a, b);
+    //cout << a << " "<< b;
+    cout << endl;
+    sort_bubble(tab, n);
+    cout << endl;
+    zamien2(tab, n);
+    drukuj(tab, n);
     
-    // wypelnij_losowe(tablica, rozmiar);
-    // drukuj(tablica, rozmiar);
-    
-    int a = 10;
-    int b = 20;
-    zamien(a, b);
-    cout << a << " " << b;
 	return 0;
 }
-
