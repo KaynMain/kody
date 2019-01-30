a07a0c65043cb749df3c30362aa94bfb5a1800b1
kl2ag1 <gabszab@vp.pl>
2019-01-30 07:19:08 +0100

    .

--- Klasa 2/cpp/wyszukaj.cpp
+++ Klasa 2/cpp/wyszukaj.cpp
@@ -3,42 +3,53 @@
  */
 
 #include <iostream>
+using namespace std;
 
-using namespace std; 
-
-void wypelnij_los(int tab[], int n) {
-    srand(time(NULL));//inicjacja generatora liczb pseudolosowych
-    for(int i = 0; i < n; i++) {
+void wypelnij_los(int tab[], int roz)
+{
+    srand(time(NULL));  
+    for(int i = 0; i < roz; i++)
+    {
         tab[i] = rand() % 101;
     }
 }
 
-void drukuj(int tab[], int n) {
-    for(int i = 0; i < n; i++) {
+void drukuj(int tab[], int roz)
+{
+    for(int i = 0; i < roz; i++)
+    {
         cout << tab[i] << " ";
     }
 }
 
-void sort_insert(int tab, int n) {
-    cout << "\nSortowanie przez wstawianie";
+void sort_insert(int tab[], int n)
+{
+    cout << "\nSortowanie przez wstawianie" << endl;
     int i, j, tmp;
-    for (i = 1; i <n; i++) { // pętla wybiera kolejne elementy zaczynając od drugiego
+    int licznik = 0;
+    for(i = 1; i < n; i++)
+    {
         tmp = tab[i];
-        j = i - 1; // 0
-        while (j >= 0 && tab[j] > tmp) {
+        j = i - 1;
+        while(j >= 0 && tab[j] >tmp)
+        {
             tab[j+1] = tab[j];
             j--;
+            licznik ++;
         }
         tab[j+1] = tmp;
     }
+    cout << "Liczba porównań: " << licznik << endl;
 }
 
-int szukaj_bin_it(int tab[], int n, int szuk) {
+int szukaj_bin_it(int tab[], int n, int szuk){
     int p, k, s;
-    p = 0; k =  n - 1;
-    while (p <= k) {
+    p = 0; 
+    k = n - 1;
+    while (p <= k){
         s = (p + k) / 2;
-        if (tab[s] == szuk) return s;
+        if (tab[s] == szuk) 
+        return s;
         else if (szuk < tab[s]) k = s - 1;
         else p = s + 1;
     }
@@ -49,8 +60,7 @@ int main(int argc, char **argv)
 {
 	int n = 20;
     int tab[n];
-    wypełnij_los(tab, n);
-    drukuj(tab, n);
+    wypelnij_los(tab, n);
     int szuk = 0;
     cout << "Podaj liczbę: ";
     cin >> szuk;
@@ -60,10 +70,9 @@ int main(int argc, char **argv)
     int indeks = szukaj_bin_it(tab, n, szuk);
     
     if (indeks >= 0)
-        cout << "\nZnaleziona na indeksie! " << indeks << endl;
+        cout << "\nZnaleziono" << indeks << endl;
     else
-        cout << "\nNie znaleziona! " << indeks << endl;
-	
-    return 0;
+        cout << "\nNie znaleziono " << indeks << endl;
+        
+	return 0;
 }
-
