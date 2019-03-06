31f694f8bcac1c9ab5b80bf1f9049cc84ca8f1fe
kl2ag1 <gabszab@vp.pl>
2019-03-06 07:14:00 +0100

    .

---
+++ Klasa 2/cpp/szyfr_przestawieniowy.cpp
@@ -0,0 +1,37 @@
+/*
+ * szyfr_przestawieniowy.cpp
+ */
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+#define MAKS 100
+
+void szyfruj(char tekst[], int klucz){
+    int ilosc = strlen(tekst);
+    cout << ilosc << endl;
+    int reszta = ilosc % klucz;
+    
+    if (reszta > 0) {
+        for(int i = ilosc; i < ilosc + klucz - reszta; i++)
+        tekst[i]= '.';
+    }
+}
+
+int main(int argc, char **argv)
+{
+	char tekst[MAKS];
+	int klucz = 0;
+    
+    cout << "Podaj tekst: " << endl;
+    cin.getline(tekst, MAKS);
+    //cout << cin.gcount()<< endl;
+    //cout << strlen(tekst) << endl;
+    cout << "Podaj klucz: " << endl;
+    cin >> klucz;
+    
+    szyfruj(tekst, klucz);
+	return 0;
+}
